6932a75f6fd3dde9601140e5861a3ebf02baf5c5
xjx1983 <xianjingxu168@163.com>
2022-05-21 16:56:07 +0800

    master  commit5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!3");
         System.out.println("Hello world!4");
+        System.out.println("master test");
     }
 }
